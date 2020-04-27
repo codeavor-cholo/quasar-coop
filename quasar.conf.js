dd0205a0b4271c6c758f513054fc0142c2dee388
nelx <jonel.recto@gmail.com>
2020-04-27 08:27:00 +0800

    layout for savings and cash advance application

--- quasar.conf.js
+++ quasar.conf.js
@@ -9,7 +9,8 @@ module.exports = function (ctx) {
     boot: [
       'axios',
       'firebase',
-      'vue-currency'
+      'vue-currency',
+      'momentjs'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
--- src/components/Admin/Withdrawals/SavingsApplications.vue
+++ src/components/Admin/Withdrawals/SavingsApplications.vue
@@ -8,20 +8,37 @@
       :pagination.sync="pagination"
       row-key="['.key']"
     >
-
-      <template v-slot:body-cell-date="props">
+      <template v-slot:body="props">
         <q-tr :props="props">
-          <q-td key="timestamp" :props="props" >
-            {{props.row.timestamp.toDate()}}
+          <q-td key="date" :props="props">
+            {{ $moment(props.row.timestamp.toDate()).format('LL') }}
           </q-td>
-        </q-tr>
-      </template>
-      <template v-slot:body-cell-actions="props">
-        <q-tr :props="props">
+
+          <q-td key="memberid" :props="props">
+            {{ props.row.MemberID }}
+          </q-td>
+
+          <q-td key="firstname" :props="props">
+            {{ props.row.FirstName }}
+          </q-td>
+
+          <q-td key="lastname" :props="props">
+            {{ props.row.LastName }}
+          </q-td>
+
+          <q-td key="designation" :props="props">
+            {{ props.row.Designation }}
+          </q-td>
+
+          <q-td key="amount" :props="props">
+            {{ props.row.Amount | currency }}
+          </q-td>
+
           <q-td key="actions" :props="props" class="q-gutter-xs">
             <q-btn dense color="positive" no-caps icon="mdi-check">Approve</q-btn>
             <q-btn dense color="negative" no-caps icon="mdi-close">Declined</q-btn>
           </q-td>
+
         </q-tr>
       </template>
     </q-table>
@@ -34,39 +51,16 @@ import { firebaseDb } from 'boot/firebase'
 import { differenceBy } from 'lodash'
 
 export default {
-  // async mounted () {
-  //   this.tableLoading = true
-  //   var datenow = new Date();
-  //   datenow.setHours(0, 0, 0, 0)
-  //   await this.$binding('TodayTransactions', firebaseDb.collection('Transactions').where('timestamp', '>', datenow).where('TransactionType', '==', 'Payment'))
-  //     .then((trans) => {
-        
-  //       this.$binding('PaidMembers', firebaseDb.collection('MemberData'))
-  //         .then((data) => {
-            
-  //           let transactions = trans
-  //           let members = data
-
-  //           let paidMembers = []
-  //           for (var i = 0; i < transactions.length; i++) {
-  //             for  (var j = 0; j < members.length; j++) {
-  //               if (transactions[i].MemberID === members[j]['.key']) {
-  //                 paidMembers.push(members[j])
-  //               }
-  //             }
-  //           }
-  //           this.tableLoading = false
-  //           this.PaidMembers = paidMembers
-  //         })
-  //     })
-  // },
+  created () {
+    this.tableLoading = true
+  },
   firestore () {
     return {
       SavingsApplications: {
         ref: firebaseDb.collection('WithdrawalApplications'),
         object: true,
-        resolve: (data) => {
-          console.log(data)
+        resolve: () => {
+          this.tableLoading = false
         }
       }
     }
@@ -95,8 +89,8 @@ export default {
         { name: 'lastname', align: 'center', label: 'Last name', field: 'LastName', sortable: true },
         { name: 'firstname', align: 'center', label: 'First name', field: 'FirstName', sortable: true },
         { name: 'designation', align: 'center', label: 'Designation', field: 'Designation', sortable: true },
-        { name: 'amount', align: 'center', label: 'Amount', field: 'Amount'},
-        { name: 'actions', align: 'center'}
+        { name: 'amount', align: 'center', label: 'Amount'},
+        { name: 'actions', align: 'center', label: 'Actions'}
       ],
       
     }
